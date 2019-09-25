18421d3138dbb9c7fb9d013655e570033c850154
MichaelStephensCPP <55512087+MichaelStephensCPP@users.noreply.github.com>
2019-09-25 20:32:26 +0000

    Update game.cpp

--- Lab4/game.cpp
+++ Lab4/game.cpp
@@ -94,33 +94,60 @@ class videogame : public game
     string difficulty;
 };
 
+void function1(game a)
+{
+    a.play();
+    a.winner();
+}
+
 int main()
 {
     game a;
     boardgame b;
     videogame c;
-    
-    //game boardgame d;
+    game d=game{b};
+    game e=game{c};
 
+while(true)
+{
     int choice;
+    char yesno;
     cout<<" Press 1 for an instance of game. \n Press 2 for an instance of board game. \n Press 3 for an instance of video game. \n Press 4 for an instance of a board game declared as a game \n Press 5 for an instance of a video game declared as a game \n";
     cin>>choice;
     if(choice==1)
     {
         a.play();
         a.winner();
+        function1(a);
     }
-    if(choice==2)
+    else if(choice==2)
     {
         b.play();
         b.winner();
+        function1(b);
     }
-    if(choice==3)
+    else if(choice==3)
     {
         c.play();
         c.winner();
+        function1(c);
     }
-
+    else if(choice==4)
+    {
+        d.play();
+        d.winner();
+        function1(d);
+    }
+    else if(choice==5)
+    {
+        e.play();
+        e.winner();
+        function1(e);
+    }
+    cout<<"Would you like to continue? (y/n)"<<endl;
+    cin>>yesno;
+    if(yesno=='n' || yesno=='N')break;
+}
 
     return 0;
 }
